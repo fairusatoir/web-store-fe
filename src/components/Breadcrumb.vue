b71f16ceee969fa15cc274d8db727c7a9228f298
Fairus Atoir <36189723+fairusatoir@users.noreply.github.com>
2023-05-15 06:58:08 +0700

    add: slicing Breadcrumb component

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- Breadcrumb Section Begin -->
+  <div class="breacrumb-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrumb-text product-more text-left">
+            <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
+            <span>{{ routeNow }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Breadcrumb Section End -->
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: "Breadcrumb",
+  props: ["routeNow"],
+};
+</script>
+
+<style></style>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,21 +1,7 @@
 <template>
   <div class="product">
     <Header />
-    <!-- Breadcrumb Section Begin -->
-    <div class="breacrumb-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="breadcrumb-text product-more text-left">
-              <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
-              <span>Detail</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Breadcrumb Section End -->
-
+    <Breadcrumb v-bind:routeNow="routeNow" />
     <!-- Product Shop Section Begin -->
     <section class="product-shop spad page-details">
       <div class="container">
@@ -110,6 +96,7 @@ import { Carousel, Slide, Navigation, Pagination } from "vue3-carousel";
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import ProductRelated from "@/components/ProductRelated.vue";
+import Breadcrumb from "@/components/Breadcrumb.vue";
 
 export default {
   name: "ProductView",
@@ -121,9 +108,11 @@ export default {
     Slide,
     Navigation,
     Pagination,
+    Breadcrumb,
   },
   data() {
     return {
+      routeNow: "Detail",
       image_default: "img/mickey1.jpg",
       thumbs: [
         "img/mickey1.jpg",
