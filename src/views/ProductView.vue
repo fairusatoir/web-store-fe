305b3073a422cf402565bb47726f4a20617a966d
Fairus Atoir <36189723+fairusatoir@users.noreply.github.com>
2023-05-14 18:27:47 +0700

    feat: adding product page

---
+++ src/views/ProductView.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="product">
+    <Header />
+    <!-- Breadcrumb Section Begin -->
+    <div class="breacrumb-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="breadcrumb-text product-more text-left">
+              <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+              <span>Detail</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Breadcrumb Section Begin -->
+
+    <!-- Product Shop Section Begin -->
+    <section class="product-shop spad page-details">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="product-pic-zoom">
+                  <img class="product-big-img" :src="image_default" alt="" />
+                </div>
+                <div class="product-thumbs">
+                  <carousel
+                    class="product-thumbs-track ps-slider"
+                    v-bind="settings"
+                    :breakpoints="breakpoints"
+                    :dots="false"
+                    :nav="false"
+                    :autoplay="5000"
+                    :wrap-around="true"
+                  >
+                    <slide
+                      class="pt thumbs-items"
+                      :class="{ active: index === activeIndex }"
+                      v-for="(imageProduct, index) in thumbs"
+                      :key="index"
+                      @click="changeImage(index)"
+                    >
+                      <img :src="imageProduct" alt="" />
+                    </slide>
+                    <template #addons>
+                      <Navigation />
+                    </template>
+                  </carousel>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="product-details">
+                  <div class="pd-title text-left">
+                    <span>oranges</span>
+                    <h3>Pure Pineapple</h3>
+                  </div>
+                  <div class="pd-desc text-left">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Corporis, error officia. Rem aperiam laborum voluptatum
+                      vel, pariatur modi hic provident eum iure natus quos non a
+                      sequi, id accusantium! Autem.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quam possimus quisquam animi, commodi, nihil voluptate
+                      nostrum neque architecto illo officiis doloremque et
+                      corrupti cupiditate voluptatibus error illum. Commodi
+                      expedita animi nulla aspernatur. Id asperiores blanditiis,
+                      omnis repudiandae iste inventore cum, quam sint molestiae
+                      accusamus voluptates ex tempora illum sit perspiciatis.
+                      Nostrum dolor tenetur amet, illo natus magni veniam quia
+                      sit nihil dolores. Commodi ratione distinctio harum
+                      voluptatum velit facilis voluptas animi non laudantium, id
+                      dolorem atque perferendis enim ducimus? A exercitationem
+                      recusandae aliquam quod. Itaque inventore obcaecati, unde
+                      quam impedit praesentium veritatis quis beatae ea atque
+                      perferendis voluptates velit architecto?
+                    </p>
+                    <h4>$495.00</h4>
+                  </div>
+                  <div class="quantity">
+                    <a href="shopping-cart.html" class="primary-btn pd-cart"
+                      >Add To Cart</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Product Shop Section End -->
+    <Footer />
+  </div>
+</template>
+
+<script>
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Navigation } from "vue3-carousel";
+// @ is an alias to /src
+
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "ProductView",
+  components: {
+    Header,
+    Footer,
+    Carousel,
+    Slide,
+    Navigation,
+  },
+  data() {
+    return {
+      image_default: "img/mickey1.jpg",
+      thumbs: [
+        "img/mickey1.jpg",
+        "img/mickey2.jpg",
+        "img/mickey3.jpg",
+        "img/mickey4.jpg",
+      ],
+      activeIndex: 0,
+      settings: {
+        itemsToShow: 3,
+        snapAlign: "start",
+      },
+    };
+  },
+  methods: {
+    changeImage(index) {
+      this.image_default = this.thumbs[index];
+      this.activeIndex = index;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.thumbs-items {
+  margin-right: 10px;
+}
+.thumbs-items img {
+  display: block;
+  width: auto;
+}
+</style>
